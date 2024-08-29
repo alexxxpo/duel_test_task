f1bff32c1931b4a9c9607acd7ab75d0cb881773c
apopov <pan@elkam.ru>
2024-08-29 08:59:49 +0500

    remove unused files

--- src/components/spell-color-picker/spellCardContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export default createContext({
-    showCardOne: false,
-    showCardTwo: false,
-    setShowCardOne: () => { },
-    setShowCardTwo: () => { }
-})
