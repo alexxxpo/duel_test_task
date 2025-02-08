479a6288efa1453295726b91588b89eca62c859d
alexxxpo <alexand-90@yandex.ru>
2025-02-08 13:52:04 +0500

    test

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@ export const DEFAULT_SPEED_PLAYER = 1
 export const MIN_CAST_SPEED = 30
 export const MAX_CAST_SPEED = 300
 export const DEFAULT_CAST_SPEED = 150
+export const DEFAULT_CAST_SPEED_2 = 150
